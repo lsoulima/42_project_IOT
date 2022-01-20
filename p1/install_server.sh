05ee7a73a9cfc2fe870b5e814ae78f591c0f3435
lsoulima <47644158+lsoulima@users.noreply.github.com>
2022-01-20 01:36:47 +0100

    update p1 scripts

--- p1/install_server.sh
+++ p1/install_server.sh
@@ -1,2 +1,55 @@
 #! /bin/bash
 
+# installation 
+
+
+yum install -y container-selinux selinux-policy-base
+rpm -i https://rpm.rancher.io/k3s-selinux-0.1.1-rc1.el7.noarch.rpm
+curl -sfL https://get.k3s.io | sh -
+
+# flannel
+
+sudo vim /etc/systemd/system/k3s.service
+    # ExecStart=/usr/local/bin/k3s \
+    #     server \
+    #     --flannel-iface 'eth1'
+
+sudo systemctl daemon-reload
+sudo systemctl restart k3s
+
+
+
+
+# installation de kubectl
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+yum install -y kubectl
+
+
+# certificat
+
+/etc/rancher/k3s/k3s.yaml
+
+
+# modification des droits
+
+chmod 644 /etc/rancher/k3s/k3s.yaml
+
+
+# premier kubectl 
+
+
+kubectl get nodes
+# -o wide
+# --kubeconfig=/etc/rancher/k3s/k3s.yaml
+# ou run avec --write-kubeconfig-mode
+
--- p1/install_worker.sh
+++ p1/install_worker.sh
@@ -1,2 +1,20 @@
 #! /bin/bash
 
+
+# ajout d'un noeud
+
+yum install -y container-selinux selinux-policy-base
+rpm -i https://rpm.rancher.io/k3s-selinux-0.1.1-rc1.el7.noarch.rpm
+curl -sfL https://get.k3s.io | K3S_TOKEN=$TOKEN K3S_URL=https://192.168.42.110:6443 sh -
+
+
+# flannel
+
+
+sudo vim /etc/systemd/system/k3s-agent.service
+    # ExecStart=/usr/local/bin/k3s \
+    #     agent \
+    #     --flannel-iface 'eth1'
+
+sudo systemctl daemon-reload
+sudo systemctl restart k3s-agent
